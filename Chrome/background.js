4656efa164bdf840982b1e078af5f57dcae7843e
dongmin <happydm09@naver.com>
2026-01-08 00:48:43 +0900

    Update background.js

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,11 +1,22 @@
-var Useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.215 Electron/22.3.2 Safari/537.36 discord/0.0.41'
+// Discord Desktop User agent
+var useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.215 Electron/22.3.2 Safari/537.36 discord/0.0.41'
 
-chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
-    for (let i = 0; i < details.requestHeaders.length; ++i) {
-        if (details.requestHeaders[i].name === 'User-Agent') {
-            details.requestHeaders[i].value = Useragent
-        }
+async function update() {
+  var rule = {
+    id: 1,
+    priority: 1,
+    action: {
+      type: "modifyHeaders",
+        requestHeaders: [
+        { header: "user-agent", operation: "set", value: useragent }
+      ]
+    },
+    condition: {
+      urlFilter: "||discord.com",
+      resourceTypes: [ "main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "webtransport", "webbundle", "other" ]
     }
-    return { requestHeaders: details.requestHeaders }
-  },
-{ urls: ["<all_urls>"] }, ["blocking", "requestHeaders"])
+  }
+  await chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [1], addRules: [rule] })
+}
+
+update()
