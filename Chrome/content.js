dd54f80d7e0b39e679c2bf56b97cf4ed43556f1c
dongmin <happydm09@naver.com>
2026-01-08 00:47:55 +0900

    Update content.js

--- Chrome/content.js
+++ Chrome/content.js
@@ -1,3 +1,42 @@
+function find(parent, selector, timeout=5000) {
+  return new Promise((resolve) => {
+    if (parent.querySelector(selector)) {
+      return resolve(parent.querySelector(selector))
+    }
+
+    var observer = new MutationObserver(() => {
+      if (parent.querySelector(selector)) {
+        observer.disconnect()
+        resolve(parent.querySelector(selector))
+      }
+    })
+
+    observer.observe(parent.body, { childList: true, subtree: true })
+    setTimeout(() => { observer.disconnect(); resolve(null); }, timeout);
+  })
+}
+
+(async () => {
+  var container = await find(document, '._57454517c20909ea-headingWrapper');
+  if (!container) { return }
+
+  var ele = await find(container, '._4bd5201c86a2042b-defaultColor')
+  if (!ele) { return }
+
+  ele.insertAdjacentHTML('beforeend', ` <button id="QuestHunter">Start Quest</button>`)
+
+  var btn = document.querySelector('#QuestHunter')
+  var cnt = 0
+
+  btn.addEventListener("click", () => {
+    if (cnt > 0) { alert('Please reload the page'); return }
+    var script = document.createElement('script')
+    script.src = chrome.runtime.getURL('main.js')
+    document.head.appendChild(script)
+    cnt++
+  })
+})()
+
 var viewport = document.querySelector('meta[name="viewport"]')
 
 if (!viewport) {
