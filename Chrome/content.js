c4b88ff9d719a2cc919d8ad61f2eded1f891abb3
dongmin <happydm09@naver.com>
2026-01-27 13:08:08 +0900

    Update content.js

--- Chrome/content.js
+++ Chrome/content.js
@@ -1,35 +1,32 @@
-function find(parent, selector, timeout=5000) {
-  return new Promise((resolve) => {
-    if (parent.querySelector(selector)) {
-      return resolve(parent.querySelector(selector))
-    }
+(() => {
+  var ele = document.body
 
-    var observer = new MutationObserver(() => {
-      if (parent.querySelector(selector)) {
-        observer.disconnect()
-        resolve(parent.querySelector(selector))
-      }
-    })
+  ele.insertAdjacentHTML('afterend', `
+  <style>
+  .qbtn {
+    bottom: 30px;
+    right: 30px;
+    position: fixed;
+    z-index: 9999;
+    background-color: #6c757d;
+    padding: 10px 20px;
+    border-radius: 10px;
+    color: white;
+  }
 
-    observer.observe(parent.body, { childList: true, subtree: true })
-    setTimeout(() => { observer.disconnect(); resolve(null); }, timeout);
-  })
-}
+  .qbtn:hover {
+    background-color: #5C636A;
+  }
+  </style>
+`)
 
-(async () => {
-  var container = await find(document, '._57454517c20909ea-headingWrapper');
-  if (!container) { return }
-
-  var ele = await find(container, '._4bd5201c86a2042b-defaultColor')
-  if (!ele) { return }
-
-  ele.insertAdjacentHTML('beforeend', ` <button id="QuestHunter">Start Quest</button>`)
+  ele.insertAdjacentHTML('afterend', `<a href='#'><span id='QuestHunter' class='qbtn'>Start Quest</span></a>`)
 
   var btn = document.querySelector('#QuestHunter')
   var cnt = 0
 
   btn.addEventListener("click", () => {
-    if (cnt > 0) { alert('Please reload the page'); return }
+    if (cnt > 0) { alert('To complete another quest, Please reload the page'); return }
     var script = document.createElement('script')
     script.src = chrome.runtime.getURL('main.js')
     document.head.appendChild(script)
